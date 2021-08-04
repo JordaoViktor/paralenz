e45851bfeb38e809ba644be1fe20979e92a84411
JordaoVictor <jordao.pinheiro@dotgroup.com.br>
2021-08-04 16:48:21 -0300

    feat: initial setup with react-navigation and styled components

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text } from "react-native";
+import styled from "styled-components/native";
+
+const Header = styled.View`
+  flex: 1;
+  background-color: red;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Header>
+      <Text>hello world !</Text>
       <StatusBar style="auto" />
-    </View>
+    </Header>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
