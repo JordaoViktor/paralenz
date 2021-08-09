e6d5d4fa7aea5218601854cd98ac241be5aa3635
JordaoVictor <jordao.pinheiro@dotgroup.com.br>
2021-08-09 01:31:40 -0300

    fix: removing reanimated v2

--- src/context/CharacterInformation/index.tsx
+++ src/context/CharacterInformation/index.tsx
@@ -1,8 +1,18 @@
-import React, {createContext, ReactNode, useState} from 'react';
+import React, {
+  createContext,
+  ReactNode,
+  useState,
+  ReactChildren,
+  ReactChild,
+} from 'react';
 
 export const CharacterInformationContext = createContext({} as any);
-
-export const CharacterInformation: ReactNode = ({children}: any) => {
+interface CharacterInformationProps {
+  children: () => ReactChildren | ReactChild;
+}
+export const CharacterInformation: ReactNode = ({
+  children,
+}: new () => CharacterInformationProps) => {
   const [characterFullInfo, setCharacterFullInfo] = useState([]);
   const [characterHomeWorld, setCharacterHomeWorld] = useState({});
 
